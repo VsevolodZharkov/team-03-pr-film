5745339b55fd7d57c746b1ef4ab6e5ccd865ef29
vsevolodjarkov <94003560+vsevolodjarkov@users.noreply.github.com>
2022-07-05 00:41:13 +0300

    save

--- src/js/index.js
+++ src/js/index.js
@@ -5,6 +5,4 @@ import { getGenresPopfilms } from './apisreq/genresandtrends';
 
 getGenresPopfilms();
 
-console.log('hello i go home');
-
 searchMovies('batman', 1).then(data => console.log(data.results));
