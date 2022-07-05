6c9e16292456246b1f0d733037b9e54ceef418ee
vsevolodjarkov <94003560+vsevolodjarkov@users.noreply.github.com>
2022-07-05 21:59:28 +0300

    save

---
+++ src/js/modal-footer/modal-footer.js
@@ -0,0 +1,20 @@
+
+	const refs = {
+		openModalBtn: document.querySelector('.footer__link'),
+		closeModalBtn: document.querySelector('[data-modal-close]'),
+		modal: document.querySelector("[data-modal-footer]"),
+	};
+console.log(refs.closeModalBtn);
+	refs.openModalBtn.addEventListener("click", openModal);
+	refs.closeModalBtn.addEventListener("click", closeModal);
+
+	function openModal(e) {
+		e.preventDefault();
+		refs.modal.classList.add("is-hidden");
+	}
+
+	function closeModal (e) {
+		e.preventDefault();
+		refs.modal.classList.remove("is-hidden");
+
+	}
