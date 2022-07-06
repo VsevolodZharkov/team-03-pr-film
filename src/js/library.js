bc42e52b321b9b1dc59de007e3faab98e2379cb8
vsevolodjarkov <94003560+vsevolodjarkov@users.noreply.github.com>
2022-07-06 19:08:28 +0300

    save

--- src/js/library.js
+++ src/js/library.js
@@ -1,25 +1,25 @@
-import { createMarkUp } from './markup/createmarkup';
-import { getGenresPopfilms } from './apisreq/genresandtrends';
-import { renderButtonsPag } from './paginaton/pagination';
-import { showModal } from './modal/modal';
-import { spiner } from './paginaton/spiner';
-import { SeachByQuery } from './header/header';
-import { getTrendMovies } from './apisreq/gettrends';
+// import { createMarkUp } from './markup/createmarkup';
+// import { getGenresPopfilms } from './apisreq/genresandtrends';
+// import { renderButtonsPag } from './paginaton/pagination';
+// import { showModal } from './modal/modal';
+// import { spiner } from './paginaton/spiner';
+// import { SeachByQuery } from './header/header';
+// import { getTrendMovies } from './apisreq/gettrends';
 
-//----------------------------------------------------------------//
-let searchData = null;
-// do ligic with seach film by name
+// //----------------------------------------------------------------//
+// let searchData = null;
+// // do ligic with seach film by name
 
-getGenresPopfilms()
-  .then(data => {
-    console.log(data);
-    createMarkUp(data.results);
-    renderButtonsPag(1, data.total_pages);
-    showModal(data);
-  })
-  .catch(er => console.log(er))
-  .finally(() => {
-    spiner.stop();
-  });
+// getGenresPopfilms()
+//   .then(data => {
+//     console.log(data);
+//     createMarkUp(data.results);
+//     renderButtonsPag(1, data.total_pages);
+//     showModal(data);
+//   })
+//   .catch(er => console.log(er))
+//   .finally(() => {
+//     spiner.stop();
+//   });
 
-SeachByQuery();
+// SeachByQuery();
