4438b5d3d32cdc27881e001b0e01908c5eb75146
VsevolodZharkov <94003560+VsevolodZharkov@users.noreply.github.com>
2022-07-06 08:13:32 +0000

    Deploying to gh-pages from @ VsevolodZharkov/team-03-pr-film@95ce2fcc8ad08ae1dff337818bbe619e65d5b7a1 🚀

---
+++ index.79788d22.js
@@ -0,0 +1,2 @@
+!function(){var e={openModalBtn:document.querySelector(".footer__link"),closeModalBtn:document.querySelector(".modal__btn-footer"),modal:document.querySelector("[data-modal-footer]")};e.openModalBtn.addEventListener("click",(function(o){o.preventDefault(),e.modal.classList.add("is-hidden")})),e.closeModalBtn.addEventListener("click",(function(o){o.preventDefault(),e.modal.classList.remove("is-hidden")}))}();
+//# sourceMappingURL=index.79788d22.js.map
