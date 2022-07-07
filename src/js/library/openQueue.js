85da04e08849bce6f683267b4433d9489d0b08fd
Volodymyr Romanenko <romanonebe@gmail.com>
2022-07-07 12:17:01 +0300

    pretest

--- src/js/library/openQueue.js
+++ src/js/library/openQueue.js
@@ -5,23 +5,23 @@ import { showLabModal } from './lab-modal';
 const watched = document.querySelector('.js-watched');
 const queue = document.querySelector('.js-queue');
 
-function openQueue () {
-	watched.classList.remove('is-active');
-	queue.classList.add('is-active');
-	const films = getMovieFromLocalStorage('queue');
-	let perPage = 20;
-	let totalPages = Math.ceil(films.length / perPage);
+function openQueue() {
+  watched.classList.remove('is-active');
+  queue.classList.add('is-active');
+  const films = getMovieFromLocalStorage('queue');
+  if (!films) {
+    return;
+  }
+  let perPage = 20;
+  let totalPages = Math.ceil(films.length / perPage);
 
-//рендер карточек
-	createMarkUpListFilm(1, films);
+  //рендер карточек
+  createMarkUpListFilm(1, films);
 
-//рендер пагинации
-	renderBtnPag(1, totalPages);
-	handlerPagination(totalPages, 'queue');
-	showLabModal(films);
+  //рендер пагинации
+  renderBtnPag(1, totalPages);
+  handlerPagination(totalPages, 'queue');
+  showLabModal(films);
 }
-
-
-
 
 export { openQueue };
