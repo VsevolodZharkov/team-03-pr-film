759636579a86f48c0c8f8d7400d952ac41f14af2
vsevolodjarkov <94003560+vsevolodjarkov@users.noreply.github.com>
2022-07-07 23:18:33 +0300

    save

--- src/js/modal-footer/modal-footer.js
+++ src/js/modal-footer/modal-footer.js
@@ -3,14 +3,16 @@
 		openModalBtn: document.querySelector('.footer__link'),
 		closeModalBtn: document.querySelector('.modal__btn-footer'),
 		modal: document.querySelector("[data-modal-footer]"),
-		
+		body: document.querySelector("body")
 	};
+	console.log(refs.body);
 	refs.openModalBtn.addEventListener("click", openModal);
 	refs.closeModalBtn.addEventListener("click", closeModal);
 
 	function openModal(e) {
 		e.preventDefault();
 		refs.modal.classList.add("is-hidden");
+		refs.body.classList.add("ishidden")
 		document.addEventListener('click', onClickBackdrop);
 		document.addEventListener('keydown', onEscClick);
 	}
@@ -18,17 +20,20 @@
 	function closeModal (e) {
 		e.preventDefault();
 		refs.modal.classList.remove("is-hidden");
+		refs.body.classList.add("ishidden")
 		removeListener();
 	}
 	function onClickBackdrop(event) {
 		if (event.target === refs.modal) {
 			refs.modal.classList.remove('is-hidden');
+			refs.body.classList.add("ishidden")
 			removeListener();
 		}
 	}
 	function onEscClick(event) {
 		if (event.code === 'Escape') {
 			refs.modal.classList.remove('is-hidden');
+			refs.body.classList.add("ishidden")
 			removeListener();
 		}
 	}
