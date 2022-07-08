c10d45b6596f51aa693c6eb06deca32f0baaa608
Volodymyr Romanenko <romanonebe@gmail.com>
2022-07-08 08:59:51 +0300

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -7,27 +7,23 @@ import { seachByQuery } from './header/header';
 // import { getTrendMovies } from './apisreq/gettrends';
 
 //----------------------------------------------------------------//
-const gallery = document.querySelector('.gallery');
+
 let searchData = null;
 let currentPage = 1;
 if (localStorage.getItem('currentPage')) {
   currentPage = localStorage.getItem('currentPage');
 }
 
-// do ligic with seach film by name
+// do ligic with seach film by
+//test
 
 getGenresPopfilms()
   .then(data => {
-    // spiner.spin(gallery);
     createMarkUp(data.results);
     renderButtonsPag(currentPage, data.total_pages);
     showModal(data);
   })
   .catch(er => console.log(er))
-  .finally(() => {
-    // spiner.stop();
-  });
+  .finally(() => {});
 
 seachByQuery();
-
-export { gallery };
