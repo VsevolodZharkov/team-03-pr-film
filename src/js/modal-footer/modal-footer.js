f8dbc1e7938966de81237342b07f0a8588e4f21b
Volodymyr Romanenko <romanonebe@gmail.com>
2022-07-10 15:01:34 +0300

    fixes

--- src/js/modal-footer/modal-footer.js
+++ src/js/modal-footer/modal-footer.js
@@ -1,44 +1,42 @@
+const refs = {
+  openModalBtn: document.querySelector('.footer__link'),
+  closeModalBtn: document.querySelector('.modal__btn-footer'),
+  modal: document.querySelector('[data-modal-footer]'),
+  body: document.querySelector('body'),
+};
 
-	const refs = {
-		openModalBtn: document.querySelector('.footer__link'),
-		closeModalBtn: document.querySelector('.modal__btn-footer'),
-		modal: document.querySelector("[data-modal-footer]"),
-		body: document.querySelector("body")
-	};
+function handlerModalFooter() {
+  refs.openModalBtn.addEventListener('click', openModal);
+  refs.closeModalBtn.addEventListener('click', closeModal);
+}
+function openModal() {
+  refs.modal.classList.add('is-openened');
+  refs.body.classList.add('ishidden');
+  document.addEventListener('click', onClickBackdrop);
+  document.addEventListener('keydown', onEscClick);
+}
 
-	function handlerModalFooter() {
-		refs.openModalBtn.addEventListener("click", openModal);
-		refs.closeModalBtn.addEventListener("click", closeModal);
-	}
-	function openModal() {
-		refs.modal.classList.add("is-hidden");
-		refs.body.classList.add("ishidden");
-		document.addEventListener('click', onClickBackdrop);
-		document.addEventListener('keydown', onEscClick);
-	}
-
-	function closeModal () {
-
-		refs.modal.classList.remove("is-hidden");
-		refs.body.classList.remove("ishidden")
-		removeListener();
-	}
-	function onClickBackdrop(event) {
-		if (event.target === refs.modal) {
-			refs.modal.classList.remove('is-hidden');
-			refs.body.classList.remove("ishidden")
-			removeListener();
-		}
-	}
-	function onEscClick(event) {
-		if (event.code === 'Escape') {
-			refs.modal.classList.remove('is-hidden');
-			refs.body.classList.remove("ishidden")
-			removeListener();
-		}
-	}
-	function removeListener() {
-		document.removeEventListener('keydown', onEscClick);
-		document.removeEventListener('click', onClickBackdrop);
-	}
-	export { handlerModalFooter };
+function closeModal() {
+  refs.modal.classList.remove('is-openened');
+  refs.body.classList.remove('ishidden');
+  removeListener();
+}
+function onClickBackdrop(event) {
+  if (event.target === refs.modal) {
+    refs.modal.classList.remove('is-openened');
+    refs.body.classList.remove('ishidden');
+    removeListener();
+  }
+}
+function onEscClick(event) {
+  if (event.code === 'Escape') {
+    refs.modal.classList.remove('is-openened');
+    refs.body.classList.remove('ishidden');
+    removeListener();
+  }
+}
+function removeListener() {
+  document.removeEventListener('keydown', onEscClick);
+  document.removeEventListener('click', onClickBackdrop);
+}
+export { handlerModalFooter };
